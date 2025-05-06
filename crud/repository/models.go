84b3000f4f9555088a78193cdaf507722e65608b
Kirill <masteryoda01010101@gmail.com>
2025-05-06 13:34:14 +0300

    interface complite

--- crud/repository/models.go
+++ crud/repository/models.go
@@ -11,7 +11,6 @@ type Currency struct {
 type CurrencyRepository interface {
 	Create(ctx context.Context, c Currency) error
 	Delete(ctx context.Context, code string) error
-	Update(ctx context.Context, c Currency) bool
+	Update(ctx context.Context, c Currency) error
 	Get(ctx context.Context, code string) (Currency, error)
-	Exists(ctx context.Context, code string) (bool, error)
 }
