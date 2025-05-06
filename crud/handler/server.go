9d90a619cbf60b7d148d676783ee3531cab2596e
Kirill <masteryoda01010101@gmail.com>
2025-05-06 12:18:47 +0300

    update handlers

--- crud/handler/server.go
+++ crud/handler/server.go
@@ -3,7 +3,7 @@ package handler
 import (
 	"context"
 	kirill_sso_v2 "currency_service/crud/proto/gen/go/kirill.sso.v2"
-	"fmt"
+	"currency_service/crud/repository"
 )
 
 type Currency struct {
@@ -13,64 +13,43 @@ type Currency struct {
 }
 type CurrencyService struct {
 	kirill_sso_v2.UnimplementedCrudServer
-	Currency []Currency
+	Repo *repository.PostgresCurrencyRepository
 }
 
-func (s *CurrencyService) Delete(ctx context.Context, request *kirill_sso_v2.DeleteCurrencyRequest) (*kirill_sso_v2.DeleteCurrencyResponse, error) {
-	for _, i := range s.Currency {
-		if i.Code == request.Code {
-			s.Currency = s.Currency[:len(s.Currency)-1]
-			return &kirill_sso_v2.DeleteCurrencyResponse{}, nil
-		}
-	}
-	return &kirill_sso_v2.DeleteCurrencyResponse{}, nil
+func (s *CurrencyService) Delete(ctx context.Context, req *kirill_sso_v2.DeleteCurrencyRequest) (*kirill_sso_v2.DeleteCurrencyResponse, error) {
+	err := s.Repo.Delete(ctx, req.Code)
+	return &kirill_sso_v2.DeleteCurrencyResponse{}, err
 }
 
 func (s *CurrencyService) CreateCurrency(ctx context.Context, req *kirill_sso_v2.CreateCurrencyRequest) (*kirill_sso_v2.CreateCurrencyResponse, error) {
-	newCurrency := Currency{
+	err := s.Repo.Create(ctx, repository.Currency{
 		Code:  req.Code,
-		Value: req.Value,
 		Rate:  req.Rate,
-	}
-	s.Currency = append(s.Currency, newCurrency)
-
-	return &kirill_sso_v2.CreateCurrencyResponse{}, nil
+		Value: req.Value,
+	})
+	return &kirill_sso_v2.CreateCurrencyResponse{}, err
 }
 
 func (s *CurrencyService) UpdateCurrency(ctx context.Context, req *kirill_sso_v2.UpdateCurrencyRequest) (*kirill_sso_v2.UpdateCurrencyResponse, error) {
-	for index, currency := range s.Currency {
-		if currency.Code == req.CurrencyUpdate.Code {
-			s.Currency[index].Rate = req.CurrencyUpdate.Code
-			s.Currency[index].Value = req.CurrencyUpdate.Rate
-		}
-	}
-	return &kirill_sso_v2.UpdateCurrencyResponse{}, nil
-}
-
-func (s *CurrencyService) AddCurrency(ctx context.Context, req *kirill_sso_v2.AddCurrencyRequest) (*kirill_sso_v2.AddCurrencyResponse, error) {
-	for _, i := range s.Currency {
-		if i.Code == req.Code {
-			return nil, fmt.Errorf("such an element already exists")
-		}
-	}
-	newCurrency := Currency{
-		Code:  req.Code,
-		Value: req.Rate,
-		Rate:  req.Date,
-	}
-
-	s.Currency = append(s.Currency, newCurrency)
-
-	return &kirill_sso_v2.AddCurrencyResponse{}, nil
+	err := s.Repo.Update(ctx, repository.Currency{
+		Code:  req.CurrencyUpdate.Code,
+		Rate:  req.CurrencyUpdate.Rate,
+		Value: req.CurrencyUpdate.Value,
+	})
+	return &kirill_sso_v2.UpdateCurrencyResponse{}, err
 }
 
 func (s *CurrencyService) GetCurrency(ctx context.Context, req *kirill_sso_v2.GetCurrencyRequest) (*kirill_sso_v2.GetCurrencyResponse, error) {
-	for _, i := range s.Currency {
-		if i.Code == req.Code {
-			return &kirill_sso_v2.GetCurrencyResponse{
-				Currency: &kirill_sso_v2.CurrencyRate{},
-			}, nil
-		}
+	c, err := s.Repo.Get(ctx, req.Code)
+	if err != nil {
+		return nil, err
 	}
-	return &kirill_sso_v2.GetCurrencyResponse{}, nil
+
+	return &kirill_sso_v2.GetCurrencyResponse{
+		Currency: &kirill_sso_v2.CurrencyRate{
+			Code:  c.Code,
+			Rate:  c.Rate,
+			Value: c.Value,
+		},
+	}, nil
 }
