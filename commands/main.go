b4d2ca86ba4d804a1caac5f78f7de36a365d1389
Kirill <masteryoda01010101@gmail.com>
2025-05-06 13:39:18 +0300

    move files

--- commands/main.go
+++
@@ -1,7 +0,0 @@
-package commands
-
-//
-// генерация файлов
-//protoc --go_out=proto/gen/go --go_opt=paths=source_relative --go-grpc_out=proto/gen/go --go-grpc_opt=paths=source_relative proto/currency.proto
-//запуск grpc
-//go run grpc/cmd/main.go --config=grpc/internal/config/local.yaml
