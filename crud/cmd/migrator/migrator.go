2b4262dc8d2ab649ed8f58e67825ad0db40726f9
Kirill <masteryoda01010101@gmail.com>
2025-05-06 22:09:48 +0300

    working migrations

--- crud/cmd/migrator/migrator.go
+++ crud/cmd/migrator/migrator.go
@@ -13,27 +13,23 @@ import (
 )
 
 func RunMigrations(connStr string, migrationsPath string) error {
-	// Получаем абсолютный путь до папки с миграциями
 	absPath, err := filepath.Abs(migrationsPath)
 	if err != nil {
 		return fmt.Errorf("failed to get absolute path: %w", err)
 	}
 	log.Println("Applying migrations from:", absPath)
 
-	// Создаём подключение к базе данных
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		return fmt.Errorf("error opening db: %w", err)
 	}
 	defer db.Close()
 
-	// Создаём драйвер для миграций
 	driver, err := postgres.WithInstance(db, &postgres.Config{})
 	if err != nil {
 		return fmt.Errorf("error creating driver: %w", err)
 	}
 
-	// Формируем путь с префиксом "file://", указывая абсолютный путь
 	m, err := migrate.NewWithDatabaseInstance(
 		"file://"+absPath,
 		"postgres", driver,
@@ -42,7 +38,6 @@ func RunMigrations(connStr string, migrationsPath string) error {
 		return fmt.Errorf("error creating migrate instance: %w", err)
 	}
 
-	// Применяем миграции
 	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
 		return fmt.Errorf("migration up failed: %w", err)
 	}
