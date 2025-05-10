5a76698969f1ba28353aa9d272cada1c026051ae
Kirill <masteryoda01010101@gmail.com>
2025-05-10 11:02:22 +0300

    fixing comments in middleware

--- gateway/internal/middleware/middleware.go
+++ gateway/internal/middleware/middleware.go
@@ -3,35 +3,43 @@ package middleware
 import (
 	"errors"
 	"github.com/golang-jwt/jwt/v5"
+	"log"
 )
 
-var jwtSecret = []byte("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0IiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxMjM0fQ.8oc-KZuuLCNwvkkpX2nFA4-YcDwcg2Y8e-WtvM--KSM")
+var jwtSecret = []byte("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTc0NjkwMjMwOH0.C8EVxabGNSAN4Z7ErGRM99UNBZ2dlbOi7J50m6BQeW4")
 
 func ValidateToken(token string) bool {
 	if token == "" {
+		log.Println("Token is empty")
 		return false
 	}
-	//token = strings.TrimPrefix(token, "Bearer ")
 
+	// убираем префикс "Bearer "
 	if len(token) <= len("Bearer ") || token[:len("Bearer ")] != "Bearer " {
+		log.Println("Token format is incorrect")
 		return false
 	}
 
-	token = token[len("Bearer "):]
+	token = token[len("Bearer "):] // Убираем префикс
 
-	tokenParce, err := jwt.Parse(token, func(j *jwt.Token) (interface{}, error) {
+	tokenParsed, err := jwt.Parse(token, func(j *jwt.Token) (interface{}, error) {
 		if _, ok := j.Method.(*jwt.SigningMethodHMAC); !ok {
-			return nil, errors.New("invalid token")
+			log.Println("Invalid token signing method")
+			return nil, errors.New("invalid token signing method")
 		}
 		return jwtSecret, nil
 	})
 
 	if err != nil {
+		log.Printf("Error parsing token: %v", err)
 		return false
 	}
 
-	if !tokenParce.Valid {
+	// проверка валидности токена
+	if !tokenParsed.Valid {
+		log.Println("Token is not valid")
 		return false
 	}
+
 	return true
 }
