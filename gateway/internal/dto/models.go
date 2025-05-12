7cd24b96c5c7172534cb5408bab675568f06142c
Kirill <masteryoda01010101@gmail.com>
2025-05-12 15:59:22 +0300

    new interface for methods

--- gateway/internal/dto/models.go
+++ gateway/internal/dto/models.go
@@ -1,9 +1,18 @@
 package dto
 
+import "net/http"
+
 type CurrencyInputCreate struct {
 	Code  string  `json:"code"`
 	Rate  string  `json:"rate"`
 	Value float64 `json:"value"`
+}
+
+type GatewayMethods interface {
+	GetCurrency(w http.ResponseWriter, r *http.Request)
+	CreateCurrency(w http.ResponseWriter, r *http.Request)
+	UpdateCurrency(w http.ResponseWriter, r *http.Request)
+	DeleteCurrency(w http.ResponseWriter, r *http.Request)
 }
 
 type CurrencyInputUpdate struct {
