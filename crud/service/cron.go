1c7f6323b1a35932c0fe5f1203ca820add1e2dc0
Kirill <masteryoda01010101@gmail.com>
2025-05-16 20:17:32 +0300

    custom errors

--- crud/service/cron.go
+++ crud/service/cron.go
@@ -5,6 +5,7 @@ import (
 	"currency_service/crud/dto"
 	"currency_service/crud/repository"
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -16,33 +17,31 @@ type CronCurrencyServer struct {
 	Repo *repository.PostgresCurrencyRepository
 }
 
-func (s *CronCurrencyServer) CronUpdateCurrencyRates() {
+func (s *CronCurrencyServer) CronUpdateCurrencyRates() error {
 	log.Println("Обновление курсов валют", time.Now())
 
 	api := os.Getenv("EXTERNAL_API")
 
 	resp, err := http.Get(api)
 	if err != nil {
-		log.Println("Ошибка при запросе курсов:", err)
-		return
+		return fmt.Errorf("Ошибка при запросе курсов", err)
 	}
 	defer resp.Body.Close()
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		log.Println("Ошибка чтения ответа:", err)
-		return
+		return fmt.Errorf("Ошибка чтения ответа:", err)
 	}
 
 	var rates dto.CronCurrencyRates
 	if err := json.Unmarshal(body, &rates); err != nil {
-		log.Println("Ошибка парсинга JSON:", err)
-		return
+		return fmt.Errorf("Ошибка парсинга JSON:", err)
 	}
 
 	for code, value := range rates.Rub {
 		if err := s.Repo.CronInsertRate(context.Background(), code, "RUB", value); err != nil {
-			log.Printf("Ошибка вставки данных для валюты %s: %v", code, err)
+			fmt.Errorf("Ошибка вставки данных для валюты %s: %v", code, err)
 		}
 	}
+	return nil
 }
